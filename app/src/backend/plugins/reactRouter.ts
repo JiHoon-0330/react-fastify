015d8c2ca6b04ba1f1bb0896d2d64bf6889fd036
JiHoon-0330 <ljihoon0330@gmail.com>
2025-02-23 16:08:12 +0900

    static 설정

--- app/src/backend/plugins/reactRouter.ts
+++ app/src/backend/plugins/reactRouter.ts
@@ -1,10 +1,29 @@
 import { reactRouterFastify } from "@mcansh/remix-fastify/react-router";
+import type {
+  FastifyInstance,
+  FastifyRequest,
+  RawServerBase,
+  RouteGenericInterface,
+} from "fastify";
 import fp from "fastify-plugin";
+
+declare module "react-router" {
+  interface AppLoadContext {
+    app: FastifyInstance;
+    req: FastifyRequest<RouteGenericInterface, RawServerBase>;
+  }
+}
 
 export default fp(
   async (app) => {
     await app.register(reactRouterFastify, {
       buildDirectory: "./dist/frontend",
+      getLoadContext: (req) => {
+        return { app, req };
+      },
+      fastifyStaticOptions: {
+        root: "./dist/frontend",
+      },
     });
   },
   { name: "reactRouter", dependencies: ["app.env", "app.gracefulShutdown"] },
--- app/src/frontend/routes/home.tsx
+++ app/src/frontend/routes/home.tsx
@@ -1,3 +1,7 @@
+import type { Route } from "./+types/home";
+
+export const loader = async ({ context }: Route.LoaderArgs) => {};
+
 export default function Home() {
   return <div>Hello World</div>;
 }
